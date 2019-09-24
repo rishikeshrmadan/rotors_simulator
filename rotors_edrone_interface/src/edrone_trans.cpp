467ecccd533a7f5ddd8c476956726579a39452a6
rishikeshrmadan <rishikeshrmadan@gmail.com>
2019-09-24 15:57:43 +0530

    edrone_big added

--- rotors_edrone_interface/src/edrone_trans.cpp
+++ rotors_edrone_interface/src/edrone_trans.cpp
@@ -19,73 +19,6 @@
  */
 
 
-// #include "rotors_edrone_interface/edrone_trans.h"
-
-// #include <mav_msgs/default_topics.h>
-
-// Edrone_Trans::Edrone_Trans() {
-//   ros::NodeHandle nh;
-//   ros::NodeHandle pnh("~");
-//   ctrl_pub_ = nh_.advertise<mav_msgs::RollPitchYawrateThrust> (
-//     mav_msgs::default_topics::COMMAND_ROLL_PITCH_YAWRATE_THRUST, 50);
-
-//   control_msg_.roll = 0;
-//   control_msg_.pitch = 0;
-//   control_msg_.yaw_rate = 0;
-//   control_msg_.thrust.x = 0;
-//   control_msg_.thrust.y = 0;
-//   control_msg_.thrust.z = 0;
-//   current_yaw_vel_ = 0;
-
-//   pnh.param("axis_direction_roll", axes_.roll_direction, 1);
-//   pnh.param("axis_direction_pitch", axes_.pitch_direction, 1);
-//   pnh.param("axis_direction_thrust", axes_.thrust_direction, 1);
-
-//   pnh.param("max_v_xy", max_.v_xy, 0.8);  // [m/s]
-//   pnh.param("max_roll", max_.roll, 8.5 * M_PI / 180.0);  // [rad]
-//   pnh.param("max_pitch", max_.pitch, 8.5 * M_PI / 180.0);  // [rad]
-//   pnh.param("max_yaw_rate", max_.rate_yaw, 45.0 * M_PI / 180.0);  // [rad/s]
-//   pnh.param("max_thrust", max_.thrust, 4.0);  // [N]
-//   pnh.param("division_factor", div_factor, 85.0);  
-//   pnh.param("v_yaw_step", v_yaw_step_, 0.05);  // [rad/s]
-//   pnh.param("v_yaw_step", v_yaw_step_, 0.05);
-//   pnh.param("thrust_weight_offset_newtons",thrust_weight_offset_newtons_,14.896);//14.896);
-//   edrone_sub_ = nh_.subscribe("/drone_command", 50, &Edrone_Trans::TransCallback, this);
-//   namespace_ = nh_.getNamespace();
-// }
-
-// void Edrone_Trans::StopMav() {
-//   control_msg_.roll = 0;
-//   control_msg_.pitch = 0;
-//   control_msg_.yaw_rate = 0;
-//   control_msg_.thrust.x = 0;
-//   control_msg_.thrust.y = 0;
-//   control_msg_.thrust.z = 0;
-// }
-
-// void Edrone_Trans::TransCallback(const edrone_client::edrone_msgs::ConstPtr& msg) {
-//   control_msg_.roll = ((msg->rcRoll-1500) * max_.roll/div_factor)*axes_.roll_direction;
-//   control_msg_.pitch = ((msg->rcPitch-1500) * max_.pitch/div_factor)* axes_.pitch_direction;
-//   control_msg_.yaw_rate = current_yaw_vel_;
-//   control_msg_.thrust.z = ((msg->rcThrottle-1500) * max_.thrust / 50.0 * axes_.thrust_direction)+thrust_weight_offset_newtons_;
-//   ros::Time update_time = ros::Time::now();
-//   control_msg_.header.stamp = update_time;
-//   control_msg_.header.frame_id = "rotors_edrone_frame";
-//   Publish();
-// }
-
-// void Edrone_Trans::Publish() {
-//   ctrl_pub_.publish(control_msg_);
-// }
-// int main(int argc, char** argv) {
-//   ros::init(argc, argv, "rotors_edrone_interface");
-//   Edrone_Trans edrone_trans;
-//   ros::spin();
-//   return 0;
-// }
-
-
-
 #include "rotors_edrone_interface/edrone_trans.h"
 
 #include <mav_msgs/default_topics.h>
@@ -112,11 +45,11 @@ Edrone_Trans::Edrone_Trans() {
   pnh.param("max_roll", max_.roll, 8.5 * M_PI / 180.0);  // [rad]
   pnh.param("max_pitch", max_.pitch, 8.5 * M_PI / 180.0);  // [rad]
   pnh.param("max_yaw_rate", max_.rate_yaw, 45.0 * M_PI / 180.0);  // [rad/s]
-  pnh.param("max_thrust", max_.thrust, 0.8);  // [N]
+  pnh.param("max_thrust", max_.thrust, 30.0);  // [N]
   pnh.param("division_factor", div_factor, 85.0);  
   pnh.param("v_yaw_step", v_yaw_step_, 0.05);  // [rad/s]
   pnh.param("v_yaw_step", v_yaw_step_, 0.05);
-  pnh.param("thrust_weight_offset_newtons",thrust_weight_offset_newtons_, 0.3);//14.896);
+  pnh.param("thrust_weight_offset_newtons",thrust_weight_offset_newtons_,14.896);//14.896);
   edrone_sub_ = nh_.subscribe("/drone_command", 50, &Edrone_Trans::TransCallback, this);
   namespace_ = nh_.getNamespace();
 }
@@ -134,12 +67,7 @@ void Edrone_Trans::TransCallback(const edrone_client::edrone_msgs::ConstPtr& msg
   control_msg_.roll = ((msg->rcRoll-1500) * max_.roll/div_factor)*axes_.roll_direction;
   control_msg_.pitch = ((msg->rcPitch-1500) * max_.pitch/div_factor)* axes_.pitch_direction;
   control_msg_.yaw_rate = current_yaw_vel_;
-  if(msg->rcThrottle>1499)
-    control_msg_.thrust.z = (((-1500+msg->rcThrottle)/100.0)*max_.thrust)+thrust_weight_offset_newtons_;
-  else
-    control_msg_.thrust.z = ((-((1500-msg->rcThrottle)/1000.0))*max_.thrust)+thrust_weight_offset_newtons_;
-  //control_msg_.thrust.z = ((msg->rcThrottle-1000)/1000.0)*max_.thrust;
-  // control_msg_.thrust.z = ((msg->rcThrottle-1500) * max_.thrust / 200.0 * axes_.thrust_direction)+thrust_weight_offset_newtons_;
+  control_msg_.thrust.z = ((msg->rcThrottle-1500) * max_.thrust / 50.0 * axes_.thrust_direction)+thrust_weight_offset_newtons_;
   ros::Time update_time = ros::Time::now();
   control_msg_.header.stamp = update_time;
   control_msg_.header.frame_id = "rotors_edrone_frame";
@@ -155,6 +83,78 @@ int main(int argc, char** argv) {
   ros::spin();
   return 0;
 }
+
+
+
+// #include "rotors_edrone_interface/edrone_trans.h"
+
+// #include <mav_msgs/default_topics.h>
+
+// Edrone_Trans::Edrone_Trans() {
+//   ros::NodeHandle nh;
+//   ros::NodeHandle pnh("~");
+//   ctrl_pub_ = nh_.advertise<mav_msgs::RollPitchYawrateThrust> (
+//     mav_msgs::default_topics::COMMAND_ROLL_PITCH_YAWRATE_THRUST, 50);
+
+//   control_msg_.roll = 0;
+//   control_msg_.pitch = 0;
+//   control_msg_.yaw_rate = 0;
+//   control_msg_.thrust.x = 0;
+//   control_msg_.thrust.y = 0;
+//   control_msg_.thrust.z = 0;
+//   current_yaw_vel_ = 0;
+
+//   pnh.param("axis_direction_roll", axes_.roll_direction, 1);
+//   pnh.param("axis_direction_pitch", axes_.pitch_direction, 1);
+//   pnh.param("axis_direction_thrust", axes_.thrust_direction, 1);
+
+//   pnh.param("max_v_xy", max_.v_xy, 0.8);  // [m/s]
+//   pnh.param("max_roll", max_.roll, 8.5 * M_PI / 180.0);  // [rad]
+//   pnh.param("max_pitch", max_.pitch, 8.5 * M_PI / 180.0);  // [rad]
+//   pnh.param("max_yaw_rate", max_.rate_yaw, 45.0 * M_PI / 180.0);  // [rad/s]
+//   pnh.param("max_thrust", max_.thrust, 1.2);  // [N]
+//   pnh.param("division_factor", div_factor, 85.0);  
+//   pnh.param("v_yaw_step", v_yaw_step_, 0.05);  // [rad/s]
+//   pnh.param("v_yaw_step", v_yaw_step_, 0.05);
+//   pnh.param("thrust_weight_offset_newtons",thrust_weight_offset_newtons_, 0.3);//14.896);
+//   edrone_sub_ = nh_.subscribe("/drone_command", 50, &Edrone_Trans::TransCallback, this);
+//   namespace_ = nh_.getNamespace();
+// }
+
+// void Edrone_Trans::StopMav() {
+//   control_msg_.roll = 0;
+//   control_msg_.pitch = 0;
+//   control_msg_.yaw_rate = 0;
+//   control_msg_.thrust.x = 0;
+//   control_msg_.thrust.y = 0;
+//   control_msg_.thrust.z = 0;
+// }
+
+// void Edrone_Trans::TransCallback(const edrone_client::edrone_msgs::ConstPtr& msg) {
+//   control_msg_.roll = ((msg->rcRoll-1500) * max_.roll/div_factor)*axes_.roll_direction;
+//   control_msg_.pitch = ((msg->rcPitch-1500) * max_.pitch/div_factor)* axes_.pitch_direction;
+//   control_msg_.yaw_rate = current_yaw_vel_;
+//   if(msg->rcThrottle>1499)
+//     control_msg_.thrust.z = (((-1500+msg->rcThrottle)/100.0)*max_.thrust)+thrust_weight_offset_newtons_;
+//   else
+//     control_msg_.thrust.z = ((-((1500-msg->rcThrottle)/1000.0))*max_.thrust)+thrust_weight_offset_newtons_;
+//   //control_msg_.thrust.z = ((msg->rcThrottle-1000)/1000.0)*max_.thrust;
+//   // control_msg_.thrust.z = ((msg->rcThrottle-1500) * max_.thrust / 200.0 * axes_.thrust_direction)+thrust_weight_offset_newtons_;
+//   ros::Time update_time = ros::Time::now();
+//   control_msg_.header.stamp = update_time;
+//   control_msg_.header.frame_id = "rotors_edrone_frame";
+//   Publish();
+// }
+
+// void Edrone_Trans::Publish() {
+//   ctrl_pub_.publish(control_msg_);
+// }
+// int main(int argc, char** argv) {
+//   ros::init(argc, argv, "rotors_edrone_interface");
+//   Edrone_Trans edrone_trans;
+//   ros::spin();
+//   return 0;
+// }
 // int main(int argc, char** argv) {
 //   ros::init(argc, argv, "rotors_edrone_interface");
 //   Edrone_Trans edrone_trans;
